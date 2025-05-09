816afec774927891a922165f3810dbe15ae954b1
samyeuh <samybouhamidi2@gmail.com>
2025-05-09 14:40:39 +0200

    c

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -1,9 +1,8 @@
-# test_app.py
 import unittest
-# For a simple example, we simply test True.
-class TestApp(unittest.TestCase):
-    def test_output(self):
-        self.assertTrue(True)
+from app import app
 
-if __name__ == "__main__":
-    unittest.main()
+class TestApp(unittest.TestCase):
+    def test_home_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
