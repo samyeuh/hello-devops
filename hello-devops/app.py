9ae5375ce676c71a1fed75b1e662a745aaf65a69
samyeuh <samybouhamidi2@gmail.com>
2025-05-22 16:10:41 +0200

    asq

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,14 +1,10 @@
-# app.py
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, CI/CD!"
-
-@app.route('/status')
-def status():
-    return "ok"
 
 if __name__ == "__main__":
     app.run()
--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -6,3 +6,6 @@ class TestApp(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
